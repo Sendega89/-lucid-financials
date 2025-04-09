1e2639a5adedb6f55d09efbb705ce39e46181b59
Sendega89 <Sendega89@gmail.com>
2025-04-09 15:05:31 +0300

    finish

--- src/components/Operand.tsx
+++ src/components/Operand.tsx
@@ -1,6 +1,7 @@
 import { FormulaElement } from '../store/formulaStore'
 
 const Operand = ({ element }: { element: FormulaElement }) => {
+
     return <span className="text-xl mx-1">{element.value}</span>
 }
 
